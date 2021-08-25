4d40091072c5421d73e62bb3b0302594dcfe50e9
Jeremiah Ang <s43500@sp.edu.sg>
2021-08-25 12:16:02 +0800

    Update commons to specify url

--- website/src/commons.js
+++ website/src/commons.js
@@ -1,1 +1,4 @@
-export const host = `http://localhost:5000`;
+let url = `http://localhost:5000`;
+if (process.env.NODE_ENV === 'production') url = 'https://ades-ca2-demo.herokuapp.com';
+
+export const host = url;
